3a43a32962e2a31da594c0df9c4335bedb4c98e1
GeekNoob04 <harshitbudhraja0@gmail.com>
2025-07-17 03:45:32 +0530

    step-10

--- src/index.ts
+++ src/index.ts
@@ -64,3 +64,33 @@ async function createEmail(email: string) {
     }
 }
 createEmail("harshitdon@gmail.com");
+async function relationships() {
+    try {
+        await client.connect();
+        const relationquery = await client.query(`
+            CREATE TABLE users(
+                id SERIAL PRIMARY KEY,
+                username VARCHAR(50) UNIQUE NOT NULL,
+                password VARCHAR(250) UNIQUE NOT NULL,
+                email VARCHAR(250) NOT NULL,
+                created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+            );
+            CREATE TABLE addresses(
+                id SERIAL PRIMARY KEY,
+                user_id INTEGER NOT NULL,
+                city VARCHAR(50) NOT NULL,
+                country VARCHAR(50) NOT NULL,
+                street VARCHAR(50) NOT NULL,
+                pincode VARCHAR(20),
+                created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+            );
+        `);
+        console.log(relationquery);
+    } catch (e) {
+        console.log("There was an error: " + e);
+    } finally {
+        await client.end();
+    }
+}
+relationships();
