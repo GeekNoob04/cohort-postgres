b83f533283d43efdc4ca2a09301e97119e8d5499
GeekNoob04 <harshitbudhraja0@gmail.com>
2025-07-19 05:04:30 +0530

    inserting in user

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,2 +1,23 @@
 import { PrismaClient } from "./generated/prisma";
 const prisma = new PrismaClient();
+
+async function insertUser(
+    username: string,
+    password: string,
+    firstName: string,
+    lastName: string
+) {
+    const res = await prisma.user.create({
+        data: {
+            username,
+            password,
+            firstName,
+            lastName,
+        },
+        select: {
+            id: true,
+        },
+    });
+    console.log(res);
+}
+async function updateUser() {}
